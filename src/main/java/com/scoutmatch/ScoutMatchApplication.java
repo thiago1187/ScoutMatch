e3f24bb1d9479da5d90b12c61e72d26d74350ff5
Thiago Alves Macena <thiago.amacena2@hotmail.com>
2025-07-10 12:06:20 -0300

    Inicializacao Spring Boot

---
+++ src/main/java/com/scoutmatch/ScoutMatchApplication.java
@@ -0,0 +1,13 @@
+package com.scoutmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoutmatch/ScoutMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoutmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
